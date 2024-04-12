ecbf9e2a32f66c8ee2ed99c678afdf5590176bd0
davidade300 <iaderinho@outlook.com>
2024-04-12 10:18:11 -0300

    first commit, property files created

---
+++ src/test/java/com/springchallenge/domainmodelandorm/DomainModelAndOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springchallenge.domainmodelandorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainModelAndOrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
